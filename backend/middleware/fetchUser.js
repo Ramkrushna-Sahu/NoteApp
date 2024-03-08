5af8cd94fa3d923eae9f2d1e28310f025d691e6b
Ramkrushna Sahu <146202519+Ramkrushna-Sahu@users.noreply.github.com>
2024-03-08 10:45:41 +0530

    Update fetchUser.js

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const JWT_SECRET ="RamkrushnaSahu"
+const JWT_SECRET =//Enter jwt secret here
 
 const fetchUser = (req,res,next)=>{
     // get the user from jwt token & add id to the req Object
